a3b3a123d1d8087e04c43d3e7991ea0fe3f49e26
Daniel Choi <dhchoi@gmail.com>
2009-10-13 00:27:01 -0400

    logic for dealing with early AM hours

--- rails/app/controllers/trips_controller.rb
+++ rails/app/controllers/trips_controller.rb
@@ -3,9 +3,8 @@ require 'json_printer'
 class TripsController < ApplicationController
   include TimeFormatting
   def index
-    @date = params[:date] || Date.today.to_s
-    @result = Trip.for(:date => @date, 
-                       :headsign => params[:headsign].gsub(/\^/, "&") , 
+    # TODO fix for am hours
+    @result = Trip.for(:headsign => params[:headsign].gsub(/\^/, "&") , 
                        :route_short_name => params[:route_short_name],
                        :transport_type => params[:transport_type].downcase.gsub(" ", "_").to_sym)
     render :json => @result.to_json
--- rails/app/models/now.rb
+++ rails/app/models/now.rb
@@ -1,11 +1,27 @@
 module Now
 
   # change this as needed for testing the app at different times of the day
+  # TODO. if early AM, use an hour over 24; e.g. 25:00:00
   def self.time
-    Time.now.strftime "%H:%M:%S"
+    value =  Time.now.strftime "%H:%M:%S"
+    # This algorithm is an imperfect compromise, because some start_times of the
+    # next day's trips are as early as 3 am, and some end_times of the trip from
+    # the prev day are after three
+    if Time.now.hour < 4
+      hour, min, sec = value.split(":")
+      hour = hour.to_i + 24
+      "#{hour}:#{min}:#{sec}"
+    else
+      value
+    end
   end
 
+  # TODO in early AM hours, use the previous day
   def self.date
-    Date.today.to_s
+    if Time.now.hour < 4
+      Date.yesterday.to_s
+    else
+      Date.today.to_s
+    end
   end
 end
