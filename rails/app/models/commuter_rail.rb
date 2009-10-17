b20f9e46cffd9995615d057376ceadd404fcc353
Daniel Choi <dhchoi@gmail.com>
2009-10-17 13:40:51 -0400

    Prepend "To " to CR trains for clarity

--- rails/app/models/commuter_rail.rb
+++ rails/app/models/commuter_rail.rb
@@ -13,12 +13,13 @@ module CommuterRail
     service_ids = Service.active_on(now.date).map(&:id)
     route = Route.first(:conditions => {:mbta_id => "CR-#{line_name}"})
 
+    line_headsign = line_headsign.sub(/^To /, '')
     results = Trip.all(:select => "trips.*, count(stoppings.id) as num_stops",
       :joins => "inner join stoppings on trips.id = stoppings.trip_id ",
       :conditions => ["route_id = ? and headsign like ? and trips.end_time > '#{now.time}' and trips.service_id in (#{service_ids.join(',')}) ", route.id, "#{line_headsign}%"],
       :group => "stoppings.trip_id",
       :order => "trips.start_time asc"
-    ).map {|trip| [trip.headsign, "Starts at #{format_time(trip.start_time)}; #{trip.num_stops} #{trip.num_stops == 1 ? 'stop' : 'stops'}" ] }
+    ).map {|trip| ["To #{trip.headsign}", "Starts at #{format_time(trip.start_time)}; #{trip.num_stops} #{trip.num_stops == 1 ? 'stop' : 'stops'}" ] }
     result = [{:route_short_name => line_name, :headsigns => results}]
   end
 
@@ -26,7 +27,7 @@ module CommuterRail
   def self.trips(options)
     now = options[:now] || Now.new
     route_short_name = "CR-#{options[:route_short_name]}"
-    headsign = options[:headsign]
+    headsign = options[:headsign].sub(/^To /, '')
 
     Trip.all(:joins => :route,
              :conditions => ["routes.mbta_id = ? and headsign LIKE ? and service_id in (?) and end_time > '#{now.time}'", route_short_name, "#{headsign}%", Service.ids_active_on(now.date)], 
@@ -46,7 +47,7 @@ module CommuterRail
       :joins => "inner join trips on trips.id = stoppings.trip_id",
       :conditions => ["stoppings.stop_id = ? and trips.route_id = ? and " + 
         "trips.service_id in (?) and trips.headsign LIKE ? " +
-        "and stoppings.arrival_time > '#{now.time}'", stopping_id, route_id, Service.ids_active_on(now.date), "#{options[:headsign]}%"],
+        "and stoppings.arrival_time > '#{now.time}'", stopping_id, route_id, Service.ids_active_on(now.date), "#{options[:headsign].sub(/^To /, '')}%"],
       :order => "stoppings.arrival_time asc"
     )
 
@@ -55,7 +56,7 @@ module CommuterRail
   def self.generate_headsigns(values)
     headsigns = Hash.new(0)
     values.each do |value|
-      headsign = value['headsign'].gsub(/\s\(Train [^)]+\)/, '')
+      headsign = "To #{value['headsign'].gsub(/\s\(Train [^)]+\)/, '')}"
       headsigns[headsign] += value['trips_remaining'].to_i
     end
     headsigns.to_a
