a715ec6c403ee8fa8fe449093a87addb71823a21
Daniel Choi <dhchoi@gmail.com>
2009-10-18 13:09:14 -0400

    date timezone fix for  t alert

--- rails/config/environment.rb
+++ rails/config/environment.rb
@@ -33,7 +33,7 @@ Rails::Initializer.run do |config|
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = 'UTC'
+  config.time_zone = "Eastern Time (US & Canada)"
   #config.gem 'haml'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
