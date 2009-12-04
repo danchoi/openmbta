e10653ea473ccc6961038f4e0b052222629720f0
Eric Richmond <eric.richmond@gmail.com>
2009-12-04 15:27:50 -0500

    changed sensor to true, turned off map type selection

--- rails/public/javascripts/tripmap.js
+++ rails/public/javascripts/tripmap.js
@@ -1,14 +1,16 @@
 function initialize() {
   var latlng = new google.maps.LatLng(-34.397, 150.644);
-  var myOptions = {
-    zoom: 8,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
   var sw_latlng = new google.maps.LatLng(sw[0], sw[1]);
   var ne_latlng = new google.maps.LatLng(ne[0], ne[1]);
   var zoom_bounds = new google.maps.LatLngBounds(sw_latlng, ne_latlng);
+ 
+ var myOptions = {
+    zoom: 8,
+    center: latlng,
+    mapTypeControl: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
   map.fitBounds(zoom_bounds)
 
   var pin = '/images/map/PinDown1.png';
