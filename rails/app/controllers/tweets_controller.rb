8c55b98ad62b854c73e01d7a049354d87be4c94f
Daniel Choi <dhchoi@gmail.com>
2009-12-13 10:39:45 -0500

    rexml hack to get working on server

--- rails/app/controllers/tweets_controller.rb
+++ rails/app/controllers/tweets_controller.rb
@@ -10,8 +10,14 @@ class TweetsController < ApplicationController
 
     @entries = []
     doc.elements.each("//entry") do |entry|
+      image = nil
+      
+      # This long winded way is required because of a weird server Ruby issue
+      entry.each_element_with_attribute("rel", "image") {|link| image = link.attributes["href"]}
+        
+       # elements["link[@rel='image']"].attributes["href"],
       @entries << { :published => DateTime.parse(entry.elements["published"].text),
-        :image => entry.elements["link[@rel='image']"].attributes["href"],
+        :image => image,
         :name => entry.elements["author/name"].text,
         :uri => entry.elements["author/uri"].text,
         :content => entry.elements["content"].text
