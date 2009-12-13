4e4dad3ba9aae118dd6b5c93171ba263dbbe029e
Daniel Choi <dhchoi@gmail.com>
2009-12-13 11:03:16 -0500

    schedule grid coloring

--- rails/app/helpers/trips_helper.rb
+++ rails/app/helpers/trips_helper.rb
@@ -1,2 +1,10 @@
 module TripsHelper
+
+  # cell value for a stop arrival time
+  def cell_value(stop_id, trip_id)
+    return nil unless trip_id
+    next_arrival = @result[:stops][stop_id][:next_arrivals].detect {|x| x[1] == trip_id }  
+    return nil unless next_arrival
+    next_arrival[0] 
+  end
 end
