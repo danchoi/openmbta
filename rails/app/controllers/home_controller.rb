6228bbe25f4f26cd30df0d6390bbecba82200644
Daniel Choi <dhchoi@gmail.com>
2010-04-30 18:33:26 -0400

    donation button

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
 
   def index
     @transport_type = "Bus"
+    if params[:donated]
+      flash.now[:notice] = "Thank you for your donation. I appreciate it. - Daniel Choi "
+    end
   end
 
 
