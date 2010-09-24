9d5694e76cf2ef12023b176130f19051bbb1333e
Daniel Choi <dhchoi@gmail.com>
2010-09-23 22:36:30 -0400

    hour formatting

--- rails/app/models/time_formatting.rb
+++ rails/app/models/time_formatting.rb
@@ -4,7 +4,7 @@ module TimeFormatting
     # "%H:%M:%S" -> 12 hour clock with am or pm
     hour, min = time.split(":")[0,2]
     hour = hour.to_i
-    suffix = 'am'
+    suffix = 'a'
     if hour > 24
       hour = hour - 24
     elsif hour == 12
@@ -14,7 +14,9 @@ module TimeFormatting
       suffix = 'a'
     elsif hour > 12
       hour = hour - 12
-      suffix = 'p'
+    elsif hour == 0
+      suffix = 'a'
+      hour = 12 # midnight
     end
     "#{hour}:#{min}#{suffix}"
   end
