f801603db8ded40aaa43978dfe79608de3bf4b2e
Daniel Choi <dhchoi@gmail.com>
2010-10-15 09:20:29 -0400

    alert to please tweet and review

--- rails/app/controllers/trips_controller.rb
+++ rails/app/controllers/trips_controller.rb
@@ -76,7 +76,10 @@ class TripsController < ApplicationController
           end
           
           # @result.merge!(:ads => "iAds") # controls whether iAds are shown
-         
+          if rand(8) == 0
+            @result.merge!({:message => {:title => "Please Support OpenMBTA", :body => "By tweeting about OpenMBTA or giving it a good review in the App Store, you will encourage us to keep making this free app better."}})
+          end
+ 
 
         else # first version
           @result = TripSet.new(:headsign => (@headsign = params[:headsign].gsub(/\^/, "&")) , 
